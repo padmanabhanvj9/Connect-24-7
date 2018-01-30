76006c2a62b8fae8823f41594d44aa2635872a73
dpraja <raja.pushpanathan@gmail.com>
2018-01-30 17:59:47 +0530

    Update PushNotification.py

--- PushNotification.py
+++ PushNotification.py
@@ -1,44 +1,38 @@
-import sys
-from urllib2 import *
-import json
+from urllib import *
 import urllib
-import request
+import json
+import sys
+import urllib.request
 from flask import Flask,request,jsonify
-
 app = Flask(__name__)
-#@app.route('/pushnotification',methods=['POST'])
-def pushnotification(request):
-    MY_API_KEY = "AIzaSyB6r90CECFsLXq3bt8WZGTOHLvsJHqFGt4"
-    
+@app.route('/pushnotification',methods=['POST'])
+def pushnotification():
+    print('my first pytho script')
+
+    MY_API_KEY="AtsbgtsqwN6RaghTXVzblL9It_LHu7OcAdZ"
     messageTitle = request.json['message']
     messageBody = request.json['body']
-    #messageTitle = "PushNotification success..."
-    #messageBody = "Appointment Confirmed"
+    #messageTitle = sys.argv[1]
+    #messageBody = sys.argv[2]
 
+    values={
+    "to" : "/topics/my_little_topic",
+    "notification" : {
+    "body" : messageBody,
+    "title" : messageTitle,
+    "icon" : "ic_launcher"
+    }
+    }
 
-    data={
-        "to" : "/topics/my_little_topic",
-        "notification" : {
-            "body" : messageBody,
-            "title" : messageTitle,
-            "icon" : "ic_cloud_white_48dp"
-        }
-    }   
+    dataAsJSON = json.dumps(values).encode('utf-8')
 
-    dataAsJSON = json.dumps(data)
-
-    requests = Request(
-        "https://gcm-http.googleapis.com/gcm/send",
-        dataAsJSON,
-        { "Authorization" : "key="+MY_API_KEY,
-          "Content-type" : "application/json"
-        }
+    request1 = urllib.request.Request(
+    "https://gcm-http.googleapis...",
+    dataAsJSON ,
+    { "Authorization" : "key="+MY_API_KEY,
+    "Content-type" : "application/json"
+    }
     )
 
-    print (urlopen(requests).read())
-    return(urlopen(requests).read())
-
-
-#if __name__ == "__main__":
- #   app.run(debug=True)
- # app.run(host="192.168.1.5",port="5000")
+    print(urllib.request.urlopen(request1).read())
+    return(urllib.request.urlopen(request1).read())
