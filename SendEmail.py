7f9d6861975facc72640a6680019f3cabace66fd
dpraja <raja.pushpanathan@gmail.com>
2018-02-03 19:35:41 +0530

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -9,12 +9,39 @@ def sendemail(request):
  sender = request.json['sender']
  receiver = request.json['receiver']
  message = request.json['message']
+ data = message.split("|")
+ print(data)
  subject = request.json['subject']
  msg = MIMEMultipart()
  msg['from'] = sender
  msg['to'] = receiver
  msg['subject'] = subject
- msg.attach(MIMEText(message,'plain'))
+ # Create the body of the message (a plain-text and an HTML version)
+ html = """\
+ <html>
+  <head></head>
+  <body>
+    <dl>
+    <dt>
+    <p><font size="2" color="black">"""+str(data[0])+"""</font></p>
+    <p><font size="4" color="blue">"""+str(data[1])+"""</font></p>
+    <dd>
+    <p><font size="2" color="black">"""+str(data[2])+"""</font></p>
+    <p><font size="2" color="black">"""+str(data[3])+"""</font></p>
+    <p><font size="2" color="black">"""+str(data[4])+"""</font></p>
+    <p><font size="2" color="black">"""+str(data[5])+"""</font></p>
+    <p><i><font size="2" color="blue">"""+str(data[6])+"""</font></i></p>
+    <p><font size="4" color="blue">"""+str(data[7])+"""</font></p>
+    </dd>
+    </dl>
+
+  </body>
+ </html>
+ """
+ 
+ msg.attach(MIMEText(msg['subject'],'plain'))
+ msg.attach(MIMEText(html,'html'))
+ 
  gmailuser = 'infocuit.aravindh@gmail.com'
  password = '0487nand'
  server = smtplib.SMTP('smtp.gmail.com',587)
