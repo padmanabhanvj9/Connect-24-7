4a9ac7e2e78427f692820b63ef8952f4f90aa5bb
dpraja <raja.pushpanathan@gmail.com>
2018-02-07 13:09:02 +0530

    Update PushNotification.py

--- PushNotification.py
+++ PushNotification.py
@@ -1,43 +1,42 @@
-from urllib import *
-import urllib
-import json
 import sys
-import urllib.request
+from urllib2 import *
+import json
+import urllib
 from flask import Flask,request,jsonify
-
 app = Flask(__name__)
+
 #@app.route('/pushnotification',methods=['POST'])
 def pushnotification(request):
+    MY_API_KEY = "AIzaSyAQDQSMLhW0ihrRWaDASWPUi-U078lUn4c"
 
-        print('my first pytho script')
+    #messageTitle = sys.argv[1]
+    #messageBody = sys.argv[2]
+    messageTitle = "PushNotification success..."
+    messageBody = "Appointment Confirmed"
 
-        MY_API_KEY="AIzaSyAQDQSMLhW0ihrRWaDASWPUi-U078lUn4c"
-        messageTitle = request.json['message']
-        messageBody = request.json['body']
-        #messageTitle = sys.argv[1]
-        #messageBody = sys.argv[2]
-
-        values={
+    data={
         "to" : "/topics/my_little_topic",
         "notification" : {
-        "body" : messageBody,
-        "title" : messageTitle,
-        "icon" : "ic_launcher"
+            "body" : messageBody,
+            "title" : messageTitle,
+            "icon" : "ic_cloud_white_48dp"
         }
-        }
+    }
 
-        dataAsJSON = json.dumps(values).encode('utf-8')
+    dataAsJSON = json.dumps(data)
 
-        request1 = urllib.request.Request(
-        "https://gcm-http.googleapis...",
-        dataAsJSON ,
+    request1 = Request(
+        "https://gcm-http.googleapis.com/gcm/send",
+        dataAsJSON,
         { "Authorization" : "key="+MY_API_KEY,
-        "Content-type" : "application/json"
+          "Content-type" : "application/json"
         }
-        )
+    )
 
-        print(urllib.request.urlopen(request1).read())
-        return(urllib.request.urlopen(request1).read())
+    print urlopen(request1).read()
+    return(urlopen(request1).read())
+
+#pushnotification()
 #if __name__ == "__main__":
  #   app.run(debug=True)
- # app.run(host="192.168.1.35",port="5000")
+  #app.run(host="192.168.1.8",port=5000)
