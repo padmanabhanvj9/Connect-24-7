32ab8c5e0eeca8aebadcab4417e061dfe2a4ace0
dpraja <raja.pushpanathan@gmail.com>
2018-02-08 19:26:24 +0530

    Update apptech.py

--- apptech.py
+++ apptech.py
@@ -29,7 +29,7 @@ from weather import webhook
 from UpdateChatbots import updatecustomerinfochatbots
 from UpdateBusinessPrimaryData import updatebusinessprimary
 from SendEmail import sendemail
-from PushNotification import pushnotification
+#from PushNotification import pushnotification
 from InsertCustomerLoginData import insertcustomerlogindata
 from QueryCustomerLoginData import QueryCustomerLoginData
 from PushNotificationAll import pushnotificationall
@@ -122,9 +122,9 @@ def GetTokenNumber():
 @app.route('/sendemail',methods=['POST'])
 def email():
    return sendemail(request)
-@app.route('/pushnotification',methods=['POST'])
-def notification():
-   return pushnotification(request)
+#@app.route('/pushnotification',methods=['POST'])
+#def notification():
+ #  return pushnotification(request)
 @app.route('/InsertCustomerLoginData',methods=['POST'])
 def customerlogindata():
    return insertcustomerlogindata(request)
