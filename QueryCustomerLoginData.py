a6602b8ba187761bb9a8ddd6802f59153a93889c
dpraja <raja.pushpanathan@gmail.com>
2018-02-09 15:36:17 +0530

    Update QueryCustomerLoginData.py

--- QueryCustomerLoginData.py
+++ QueryCustomerLoginData.py
@@ -3,8 +3,8 @@ import json
 import psycopg2
 from flask import Flask,request
 app = Flask(__name__)
-@app.route('/QueryCustomerLoginData',methods=['GET'])
-def QueryCustomerLoginData():
+#@app.route('/QueryCustomerLoginData',methods=['GET'])
+def QueryCustomerLoginData(request):
     con = psycopg2.connect(user='quywvejawxhnse',password='065fe8ac62d76caa061d1e517b2f0107b5776f767037c2e29cad16c259a771cf',host='ec2-176-34-113-15.eu-west-1.compute.amazonaws.com',port='5432',database='d3opaj0jiqsm0h')
     cur = con.cursor()
 
@@ -20,6 +20,6 @@ def QueryCustomerLoginData():
     return( json.dumps(result,indent=3,default=myconverter))
     con.close()          
      
-if __name__ == "__main__":
+#if __name__ == "__main__":
   # app.run(debug=True)
-  app.run(host="192.168.1.8",port=5000)
+ # app.run(host="192.168.1.8",port=5000)
