3bf8e04a6e8fca9936ffeadbd75b28290a5c2578
dpraja <raja.pushpanathan@gmail.com>
2018-02-15 17:25:34 +0530

    Update PushNotificationAll.py

--- PushNotificationAll.py
+++ PushNotificationAll.py
@@ -1,17 +1,45 @@
-
-import urllib.request
-
+import sys
+#from urllib2 import *
+#import urllib.request  as urllib2 
+#from urllib.request import urlopen
+from urllib2 import urlopen
+import json
+import urllib
 from flask import Flask,request,jsonify
 app = Flask(__name__)
 
 #@app.route('/pushnotificationall',methods=['POST'])
 def pushnotificationall(request):
+    MY_API_KEY = "AIzaSyAQDQSMLhW0ihrRWaDASWPUi-U078lUn4c"
 
+    #messageTitle = sys.argv[1]
+    #messageBody = sys.argv[2]
+    messageTitle = request.json['message']
+    messageBody = request.json['body']
+    print(messageBody)
+    data={
+        "to" : "/topics/my_little_topic",
+        "notification" : {
+            "body" : messageBody,
+            "title" : messageTitle,
+            "icon" : "ic_cloud_white_48dp"
+        }
+    }
 
-    req = urllib.request.Request('http://www.voidspace.org.uk')
-    response = urllib.request.urlopen(req)
-    the_page = response.read()
-    print(the_page)
-    return(the_page)
-    
+    dataAsJSON = json.dumps(data)
 
+    request1 = Request(
+        "https://gcm-http.googleapis.com/gcm/send",
+        dataAsJSON,
+        { "Authorization" : "key="+MY_API_KEY,
+          "Content-type" : "application/json"
+        }
+    )
+
+    print (urlopen(request1).read())
+    return(urlopen(request1).read())
+
+#pushnotification()
+#if __name__ == "__main__":
+ #   app.run(debug=True)
+#  app.run(host="192.168.1.2",port=5000)
