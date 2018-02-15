6e42be6ac7a6ec271d151fe61b2894da3577230d
dpraja <raja.pushpanathan@gmail.com>
2018-02-15 17:29:57 +0530

    Update PushNotificationAll.py

--- PushNotificationAll.py
+++ PushNotificationAll.py
@@ -2,7 +2,7 @@ import sys
 #from urllib2 import *
 #import urllib.request  as urllib2 
 #from urllib.request import urlopen
-from urllib2 import urlopen
+#from urllib2 import urlopen
 import json
 import urllib
 from flask import Flask,request,jsonify
