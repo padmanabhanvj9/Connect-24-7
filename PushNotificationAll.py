d0b4903bfa56fa5909e4305c943980ac373bfac5
dpraja <raja.pushpanathan@gmail.com>
2018-02-15 17:47:54 +0530

    Update PushNotificationAll.py

--- PushNotificationAll.py
+++ PushNotificationAll.py
@@ -1,5 +1,10 @@
 import sys
-from urllib.request import urlopen
+#from urllib2 import *
+#from urllib2 import urlopen
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+#from urllib.request import urlopen
 import json
 import urllib
 from flask import Flask,request,jsonify
@@ -8,12 +13,13 @@ app = Flask(__name__)
 #@app.route('/pushnotificationall',methods=['POST'])
 def pushnotificationall(request):
     MY_API_KEY = "AIzaSyAQDQSMLhW0ihrRWaDASWPUi-U078lUn4c"
+    print(MY_API_KEY)
 
     #messageTitle = sys.argv[1]
     #messageBody = sys.argv[2]
     messageTitle = request.json['message']
     messageBody = request.json['body']
-    print(messageBody)
+    print(messageBody,messageTitle)
     data={
         "to" : "/topics/my_little_topic",
         "notification" : {
@@ -22,9 +28,9 @@ def pushnotificationall(request):
             "icon" : "ic_cloud_white_48dp"
         }
     }
-
+    print(data)
     dataAsJSON = json.dumps(data)
-
+    print(dataAsJSON)
     request1 = Request(
         "https://gcm-http.googleapis.com/gcm/send",
         dataAsJSON,
@@ -32,7 +38,7 @@ def pushnotificationall(request):
           "Content-type" : "application/json"
         }
     )
-
+    print(request1)
     print (urlopen(request1).read())
     return(urlopen(request1).read())
 
