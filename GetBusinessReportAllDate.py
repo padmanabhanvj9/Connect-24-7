fc6914c3de5c67f883ed087b6bbec25dc9d3646c
dpraja <raja.pushpanathan@gmail.com>
2018-02-27 15:13:11 +0530

    Update GetBusinessReportAllDate.py

--- GetBusinessReportAllDate.py
+++ GetBusinessReportAllDate.py
@@ -70,7 +70,7 @@ def getbusinessreportalldate(request):
           #d[name] = e
           x +=1
      print(values)
-     return(json.dumps({"business_name":d},indent =2))    
+     return(json.dumps({"Business Group":d},indent =2))    
      con.close()    
 #if __name__ == "__main__":
     #app.run(debug=True)
