6e7bc1ca2ada4f0292d6a5dcd19d4c1aad376107
dpraja <raja.pushpanathan@gmail.com>
2018-03-04 19:52:51 +0530

    Update SendEmailAll.py

--- SendEmailAll.py
+++ SendEmailAll.py
@@ -7,25 +7,14 @@ from email.mime.multipart import MIMEMultipart
 from flask import Flask,request,jsonify
 app = Flask(__name__)
 #@app.route('/sendemailall',methods=['POST'])
-def sendemailall(request):
+def callfn(sql):
  try:
       con = psycopg2.connect(user='quywvejawxhnse',password='065fe8ac62d76caa061d1e517b2f0107b5776f767037c2e29cad16c259a771cf',host='ec2-176-34-113-15.eu-west-1.compute.amazonaws.com',port='5432',database='d3opaj0jiqsm0h')
       cur = con.cursor()
  except psycopg2.Error :
       return (json.dumps({'Status': 'Failure','Message':'DB connection Error'}, sort_keys=True, indent=4))
-
- customer_appointment_date_from = request.json['appointment_date_from'] 
- customer_appointment_date_to = request.json['appointment_date_to']     
- sender = request.json['sender']
- business_id = request.json['business_id']
- if len(customer_appointment_date_from) is 0:
-      Today_date = datetime.datetime.utcnow().date().strftime('%Y-%m-%d')
-      customer_appointment_date_from = Today_date 
-      customer_appointment_date_to = Today_date 
-
- 
- mail = []
- sql = ("select customer_email from customer_details where business_id = "+business_id+"and customer_appointment_date between '"+customer_appointment_date_from+"' and '"+customer_appointment_date_to+"'")
+ sender = request.json['sender']   
+ mail = []   
  cur.execute(sql)
  result = cur.fetchall()
  for field in result:
@@ -88,7 +77,22 @@ def sendemailall(request):
      server.quit()
  print ("the message has been sent successfully") 
 
- return(json.dumps({'Message': 'Message Send Successfully'}, sort_keys=True, indent=4))
-#if __name__ == "__main__":
- #   app.run(debug=True)
-  #app.run(host="192.168.1.6",port=5000)
+ return(json.dumps({'Message': 'Message Send Successfully'}, sort_keys=True, indent=4))    
+def sendemailall(request):
+
+ customer_appointment_date_from = request.json['appointment_date_from'] 
+ customer_appointment_date_to = request.json['appointment_date_to']     
+ business_id = request.json['business_id']
+ if len(customer_appointment_date_from) is 0:
+      Today_date = datetime.datetime.utcnow().date().strftime('%Y-%m-%d')
+      customer_appointment_date_from = Today_date 
+      customer_appointment_date_to = Today_date
+ if request.json.get('status'):
+     status = request.json['status']
+     sql = ("select customer_email from customer_details where business_id = "+business_id+" and customer_appointment_date between '"+customer_appointment_date_from+"' and '"+customer_appointment_date_to+"' and customer_current_status in ('"+status+"')")
+     print(sql)
+     return(callfn(sql))
+ else:
+     sql = ("select customer_email from customer_details where business_id = "+business_id+" and customer_appointment_date between '"+customer_appointment_date_from+"' and '"+customer_appointment_date_to+"'")
+     print(sql)
+     return(callfn(sql))
