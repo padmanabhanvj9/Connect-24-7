63146208d04481266797fb774b38f08395866526
dpraja <raja.pushpanathan@gmail.com>
2018-03-29 16:50:03 +0530

    Delete FetchRoomsAvailabilityandPrice.py

--- FetchRoomsAvailabilityandPrice.py
+++
@@ -1,8 +0,0 @@
-from sqlwrapper import dbget
-import json
-def fetchroomsavailabilityandprice(request):
-    result = dbget('select * from ivr_room_availability_rate')
-    return(result) 
-def fetchpromotionalmessage(request):
-    result = dbget('select * from ivr_promotional_cancellation_message')
-    return(result) 
