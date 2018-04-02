cbdadb5c02a7ac2d96a23e69c6ad5b8494674be8
dpraja <raja.pushpanathan@gmail.com>
2018-04-02 11:08:44 +0530

    Update CalculateTotalChargesAndRetrieveConfirmationNumber.py

--- CalculateTotalChargesAndRetrieveConfirmationNumber.py
+++ CalculateTotalChargesAndRetrieveConfirmationNumber.py
@@ -8,7 +8,7 @@ def calculatetotalchargesandretrieveconfirmationnumber(request):
     customer_arrival_date = request.json["customer_arrival_date"]
     customer_depature_date = request.json["customer_depature_date"]
     customer_room_type = request.json["customer_room_type"]
-    d,e = {},[]
+    d,e,d1,d2 = {},[],{},{}
     e = ['room_count_available','room_rate','currency']
     d['room_type'] =  customer_room_type
     result = (gensql('select','ivr_room_availability_rate',e,d))
@@ -30,4 +30,8 @@ def calculatetotalchargesandretrieveconfirmationnumber(request):
     Total_amt = night * room_rate
     print(Total_amt,type(Total_amt))
     conf_no = (random.randint(1000000000,9999999999))
+    room_available -= 1
+    d1['room_count_available'] = room_available
+    d2['room_type'] = customer_room_type
+    print(gensql('update','ivr_room_availability_rate',d1,d2))
     return(json.dumps({"Return Value":{"Total Amount":Total_amt,"currency":currency,"Confirmation Number":conf_no}},indent=2))
