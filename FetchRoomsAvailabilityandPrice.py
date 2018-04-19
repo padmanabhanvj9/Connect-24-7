1207e52f3b51b383e053453ac30ff8072f6f2d5a
dpraja <raja.pushpanathan@gmail.com>
2018-04-19 11:19:07 +0530

    Update FetchRoomsAvailabilityandPrice.py

--- FetchRoomsAvailabilityandPrice.py
+++ FetchRoomsAvailabilityandPrice.py
@@ -3,7 +3,7 @@ import json
 def fetchroomsavailabilityandprice(request):
     result = dbget('select * from ivr_room_availability_rate')
     result = json.loads(result)
-    result = result[0]
+    #result = result[0]
     return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
                       "Status Code": "200", "Return Value":result},indent=2))
 
