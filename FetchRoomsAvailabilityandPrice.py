77b0a0c60103e0174f307614d5b718a0eb005f97
dpraja <raja.pushpanathan@gmail.com>
2018-04-21 18:31:35 +0530

    Update FetchRoomsAvailabilityandPrice.py

--- FetchRoomsAvailabilityandPrice.py
+++ FetchRoomsAvailabilityandPrice.py
@@ -1,12 +1,18 @@
-from sqlwrapper import dbget
+from sqlwrapper import dbget,gensql
 import json
+import flask
 def fetchroomsavailabilityandprice(request):
-    result = dbget('select * from ivr_room_availability_rate')
-    result = json.loads(result)
-    #result = result[0]
-    return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+    if flask.request.method == 'POST':
+       b_id = request.json
+       result = gensql('select','ivr_room_availability_rate','*',b_id)
+       result = json.loads(result)
+       return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
                       "Status Code": "200", "Return Value":result},indent=2))
-
+    else:
+      result = dbget('select * from ivr_room_availability_rate')
+      result = json.loads(result)
+      return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+                      "Status Code": "200", "Return Value":result},indent=2)) 
     
 def fetchpromotionalmessage(request):
     result = dbget('select * from ivr_promotional_cancellation_message')
