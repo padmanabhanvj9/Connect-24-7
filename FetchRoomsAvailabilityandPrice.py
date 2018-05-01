21c2b7139b5e6dab2d87856f035d832ee863d046
dpraja <raja.pushpanathan@gmail.com>
2018-05-01 08:06:15 +0530

    Update FetchRoomsAvailabilityandPrice.py

--- FetchRoomsAvailabilityandPrice.py
+++ FetchRoomsAvailabilityandPrice.py
@@ -1,24 +1,33 @@
-from sqlwrapper import dbget,gensql
+from sqlwrapper import dbget
 import json
-import flask
 def fetchroomsavailabilityandprice(request):
-    if flask.request.method == 'POST':
-       b_id = request.json
-       result = gensql('select','ivr_room_availability_rate','*',b_id)
-       result = json.loads(result)
-       return(json.dumps({"ServiceStatus": "Success",
-                      "ServiceMessage": "Success", "Return Value":result},indent=2))
-    else:
-      result = dbget('select * from ivr_room_availability_rate')
-      result = json.loads(result)
-      return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
-                      "Status Code": "200", "Return Value":result},indent=2)) 
+    result = dbget('select * from ivr_room_availability_rate')
+    print(result,type(result))
+    result = json.loads(result)
+    #result = result[0]
+    print(result,type(result))
+    #return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+    #                  "Status Code": "200", "Return Value":result},indent=2))
+
+    dict1 = [{"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+                      "Status Code": "200"}]
+    print(dict1,type(dict1))
+    result = result+dict1
+    print(result,type(result))
+    #dict1.update(result)
+    #print(dict1)
+    return(json.dumps(result))
     
 def fetchpromotionalmessage(request):
     result = dbget('select * from ivr_promotional_cancellation_message')
     result = json.loads(result)
     result = result[0]
-    return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
-                      "Status Code": "200", "Return Value":result},indent=2))
-
+    #return(json.dumps({"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+     #                 "Status Code": "200", "Return Value":result},indent=2))
+    dict1 = {"Return":"Record Retrieved Successfully","Return Code":"RRS", "Status": "Success",
+                      "Status Code": "200"}
+    print(dict1,type(dict1))
+    dict1.update(result)
+    print(dict1)
+    return(json.dumps(dict1))
    
