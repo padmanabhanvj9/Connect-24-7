1e42e9d7a65cd360702914fc052bc5d95794923a
dpraja <raja.pushpanathan@gmail.com>
2018-05-02 16:34:48 +0530

    Update UpdateCustomerLangSelected.py

--- UpdateCustomerLangSelected.py
+++ UpdateCustomerLangSelected.py
@@ -10,11 +10,11 @@ def updatecustomerlangselected(request):
 
     if len(tab1) > 2 :
         print(tab1)
-        return(json.dumps({"Return":"Language Already Selected","Return Code":"LAS","Status": "Success",
-                      "Status Code": "200"},indent=2))
+        return(json.dumps({"Return":"Language Already Selected","Return_Code":"LAS","Status": "Success",
+                      "Status_Code": "200"},indent=2))
     d['customer_language_pref'] = lang
     result = gensql('insert','ivr_customer_profile',d)
     
-    return(json.dumps({"Return":"Record Inserted Successfully","Return Code":"RIS","Status": "Success",
-                      "Status Code": "200"},indent=2))
+    return(json.dumps({"Return":"Record Inserted Successfully","Return_Code":"RIS","Status": "Success",
+                      "Status_Code": "200"},indent=2))
 
