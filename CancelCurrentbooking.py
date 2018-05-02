526b6455c265597d0f43636edde06f2670d765dc
dpraja <raja.pushpanathan@gmail.com>
2018-05-02 15:42:59 +0530

    Update CancelCurrentbooking.py

--- CancelCurrentbooking.py
+++ CancelCurrentbooking.py
@@ -8,8 +8,8 @@ def cancelcurrentbooking(request):
     d['customer_confirmation_number'] = no
     res = (gensql('select','ivr_room_customer_booked','*',d))
     if len(res) == 2 :
-        return(json.dumps({"Return":"Invalid Confirmation Number","Return Code":"ICN","Status": "Success",
-                      "Status Code": "200"},indent=2))  
+        return(json.dumps({"Return":"Invalid Confirmation Number","Return_Code":"ICN","Status": "Success",
+                      "Status_Code": "200"},indent=2))  
     result  = json.loads(res)
     data = result[0]
     date = data['customer_arrival_date']
@@ -18,8 +18,8 @@ def cancelcurrentbooking(request):
     arrival_date = datetime.datetime.strptime(date, '%Y-%m-%d')
     today_date = datetime.datetime.utcnow()
     if arrival_date < today_date:
-       return(json.dumps({"Return":"Can't Cancel Reservation","Return Code":"CCR","Status": "Success",
-                      "Status Code": "200"},indent=2))
+       return(json.dumps({"Return":"Can't Cancel Reservation","Return_Code":"CCR","Status": "Success",
+                      "Status_Code": "200"},indent=2))
     s = {}
     s['customer_booked_status'] = "canceled"    
     print(gensql('update','ivr_room_customer_booked',s,d))
@@ -35,8 +35,8 @@ def cancelcurrentbooking(request):
     print(room_count)
     s1['room_count_available'] = room_count
     print(gensql('update','ivr_room_availability_rate',s1,r))
-    return(json.dumps({"Return":"Reservation Canceled Sucessfully","Return Code":"RCS","Status": "Success",
-                      "Status Code": "200"},indent=2))
+    return(json.dumps({"Return":"Reservation Canceled Sucessfully","Return_Code":"RCS","Status": "Success",
+                      "Status_Code": "200"},indent=2))
   
    
     
