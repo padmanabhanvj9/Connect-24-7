2af2fca7d5fd5a1ebde38a565fa7c286d63c110d
dpraja <raja.pushpanathan@gmail.com>
2018-05-07 18:40:01 +0530

    Update AvailableRoomCount.py

--- AvailableRoomCount.py
+++ AvailableRoomCount.py
@@ -5,6 +5,6 @@ def availableroomcount(request):
     d = request.json
     print(d)
     print(d['business_id'])
-    res = json.loads(dbget("select available_count,room_name,room_type from extranet_availableroom join extranet_room_list on extranet_room_list.id = extranet_availableroom.id where room_date = '"+d['available_date']+"'"))
+    res = json.loads(dbget("select available_count,room_type from extranet_availableroom join extranet_room_list on extranet_room_list.id = extranet_availableroom.id where room_date = '"+d['available_date']+"'"))
     print(res,type(res))
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Available_Rooms":res},indent=2))
