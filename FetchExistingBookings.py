80577ac369f89c33afb3871654ac980589872352
dpraja <raja.pushpanathan@gmail.com>
2018-05-10 12:12:57 +0530

    Update FetchExistingBookings.py

--- FetchExistingBookings.py
+++ FetchExistingBookings.py
@@ -1,14 +1,14 @@
-from sqlwrapper import gensql
+from sqlwrapper import gensql,dbget
 import json
 import datetime
 #from datetime import datetime
 def fetchexistingbookings(request):
     today_date = datetime.datetime.utcnow().date()
     #no = request.args["conf_no"]
-    no = request.json['conf_no']
+    no = request.json['mobile']
     d = {}
-    d['customer_confirmation_number'] = no
-    result = (gensql('select','ivr_room_customer_booked','*',d))
+    d['customer_mobile'] = no
+    result = (dbget("select * from ivr_room_customer_booked  where customer_mobile='"+no+"' order by customer_arrival_date limit 1"))
     result = json.loads(result)
     result = result[0]
     print(result)
@@ -20,4 +20,6 @@ def fetchexistingbookings(request):
     dict1.update(result)
     print(dict1)
     return(json.dumps(dict1))
+   
+
    
