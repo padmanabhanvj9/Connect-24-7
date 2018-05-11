46f7932bedb4e5b4402dd0bbdceda92e81c36ecb
dpraja <raja.pushpanathan@gmail.com>
2018-05-11 16:40:40 +0530

    Update CalculateTotalChargesAndRetrieveConfirmationNumber.py

--- CalculateTotalChargesAndRetrieveConfirmationNumber.py
+++ CalculateTotalChargesAndRetrieveConfirmationNumber.py
@@ -9,7 +9,7 @@ def calculatetotalchargesandretrieveconfirmationnumber(request):
        customer_name = request.json["customer_name"]
     else:
        customer_name  = "Null"
-    l['customer_name'] = customer_name  
+    l['customer_name'] = customer_name   
     customer_mobile = request.json["customer_mobile"]        
     customer_arrival_date = request.json["customer_arrival_date"]
     customer_depature_date = request.json["customer_depature_date"]
@@ -18,21 +18,21 @@ def calculatetotalchargesandretrieveconfirmationnumber(request):
     print(customer_arrival_date,customer_depature_date)
     today_date = datetime.datetime.utcnow().date()
     year = str(today_date.year)
-    if int(customer_arrival_date[2:]) == today_date.month :
-        if int(customer_arrival_date[0:2]) < today_date.day :
+    if int(customer_arrival_date[0:2]) == today_date.month :
+        if int(customer_arrival_date[2:]) < today_date.day :
            year = str(today_date.year+1)
            print("year",year,type(year))
-    elif int(customer_arrival_date[2:]) < today_date.month :
+    elif int(customer_arrival_date[0:2]) < today_date.month :
         year = str(today_date.year+1)
-    customer_arrival_date = year+'-'+customer_arrival_date[2:]+'-'+customer_arrival_date[0:2]
+    customer_arrival_date = year+'-'+customer_arrival_date[0:2]+'-'+customer_arrival_date[2:]
     l['customer_arrival_date'] = customer_arrival_date
-    if int(customer_depature_date[2:]) == today_date.month :
-        if int(customer_depature_date[0:2]) < today_date.day :
+    if int(customer_depature_date[0:2]) == today_date.month :
+        if int(customer_depature_date[2:]) < today_date.day :
            year = str(today_date.year+1)
            print("year",year,type(year))
-    elif int(customer_depature_date[2:]) < today_date.month :
+    elif int(customer_depature_date[0:2]) < today_date.month :
         year = str(today_date.year+1)
-    customer_depature_date = year+'-'+customer_depature_date[2:]+'-'+customer_depature_date[0:2]
+    customer_depature_date = year+'-'+customer_depature_date[0:2]+'-'+customer_depature_date[2:]
     l['customer_depature_date'] = customer_depature_date
     print(customer_arrival_date,customer_depature_date)    
     e = ['room_count_available','room_rate','currency']
