3a2c472fee04a9c4a54efd2bc101a1d11a8d230f
dpraja <raja.pushpanathan@gmail.com>
2018-05-12 18:21:31 +0530

    Update RatesInsertAndUpdate.py

--- RatesInsertAndUpdate.py
+++ RatesInsertAndUpdate.py
@@ -13,14 +13,32 @@ def ratesinsertandupdate(request):
       sql = json.loads(dbget("select count(*) from extranet_availableroom where id = '"+str(res[0]['id'])+"'\
                              and room_date = '"+i['date']+"' "))
       print(sql[0]['count'])
-      if sql[0]['count'] == 0:	
-         dbput("INSERT INTO public.extranet_availableroom(\
+      if sql[0]['count'] == 0:
+         if  i['Available_Room_Count'] == "" and i['Price'] == "":
+             pass
+         elif i['Available_Room_Count'] == "":
+           dbput("INSERT INTO public.extranet_availableroom(\
+	        id, room_date,room_rate)\
+	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"',"+str(i['Price'])+")")
+         elif i['Price'] == "":
+           dbput("INSERT INTO public.extranet_availableroom(\
+	        id, room_date, available_count)\
+	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"', "+str(i['Available_Room_Count'])+")")
+         else:
+             dbput("INSERT INTO public.extranet_availableroom(\
 	        id, room_date, available_count, room_rate)\
 	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"', "+str(i['Available_Room_Count'])+", "+str(i['Price'])+")")
       else:
-         dbput("UPDATE public.extranet_availableroom\
+         if i['Available_Room_Count'] == "" and  i['Price'] == "":
+             pass
+         elif i['Available_Room_Count'] == "": 
+           dbput("UPDATE public.extranet_availableroom set room_rate="+str(i['Price'])+" WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"'") 
+         elif i['Price'] == "":
+            dbput("UPDATE public.extranet_availableroom set available_count="+str(i['Available_Room_Count'])+" WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"' ")
+         else:
+             dbput("UPDATE public.extranet_availableroom\
 	        set available_count="+str(i['Available_Room_Count'])+", room_rate="+str(i['Price'])+"\
-	        WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"'") 
-           
-         print(i['date'])    
+	        WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"'")  
+             
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
+
