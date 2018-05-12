7befa72066c384adcaa35fa3b78c3503347b08fc
dpraja <raja.pushpanathan@gmail.com>
2018-05-12 18:11:14 +0530

    Update InsertRatesandAvailability.py

--- InsertRatesandAvailability.py
+++ InsertRatesandAvailability.py
@@ -1,22 +1,43 @@
 import json
 import datetime
-from sqlwrapper import gensql,dbfetch,dbget
-def insertratesandavailability(request):
+from sqlwrapper import gensql,dbget,dbput
+def ratesinsertandupdate(request):
     d = request.json
-    #print(d)
-    e = { k : v for k,v in d.items() if k in ('business_id','room_type','room_name')}
-    f = { k : v for k,v in d.items() if k not in ('business_id','room_type','room_name')}
-    #print(e)
-    #print(f)
+    e = { k : v for k,v in d.items() if k in ('business_id','room_type')}
+    f = { k : v for k,v in d.items() if k in ('rooms')}
     res = json.loads(gensql('select','extranet_room_list','id',e))
-    #print(res[0]['id'],type(res[0]['id']))
-    f['id'] = res[0]['id']
-    g = { k : v for k,v in f.items() if k  in ('id','room_date')} 
-    result = json.loads(gensql('select','extranet_availableroom','count(*)',g))
-    print(result[0]['count'],type(result[0]['count']))
-    if result[0]['count'] == 0:
-        sql = gensql('insert','extranet_availableroom',f)
-        print(sql)
-        return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Record Inserted"},indent=2))
-    else:
-        return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Record Already Exists"},indent=2))
+    print(res[0]['id'],type(res[0]['id']))
+    f = f['rooms']
+    for i in f:
+      print(i)  
+      sql = json.loads(dbget("select count(*) from extranet_availableroom where id = '"+str(res[0]['id'])+"'\
+                             and room_date = '"+i['date']+"' "))
+      print(sql[0]['count'])
+      if sql[0]['count'] == 0:
+         if  i['Available_Room_Count'] == "" and i['Price'] == "":
+             pass
+         elif i['Available_Room_Count'] == "":
+           dbput("INSERT INTO public.extranet_availableroom(\
+	        id, room_date,room_rate)\
+	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"',"+str(i['Price'])+")")
+         elif i['Price'] == "":
+           dbput("INSERT INTO public.extranet_availableroom(\
+	        id, room_date, available_count)\
+	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"', "+str(i['Available_Room_Count'])+")")
+         else:
+             dbput("INSERT INTO public.extranet_availableroom(\
+	        id, room_date, available_count, room_rate)\
+	        VALUES ("+str(res[0]['id'])+", '"+i['date']+"', "+str(i['Available_Room_Count'])+", "+str(i['Price'])+")")
+      else:
+         if i['Available_Room_Count'] == "" and  i['Price'] == "":
+             pass
+         elif i['Available_Room_Count'] == "": 
+           dbput("UPDATE public.extranet_availableroom set room_rate="+str(i['Price'])+" WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"'") 
+         elif i['Price'] == "":
+            dbput("UPDATE public.extranet_availableroom set available_count="+str(i['Available_Room_Count'])+" WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"' ")
+         else:
+             dbput("UPDATE public.extranet_availableroom\
+	        set available_count="+str(i['Available_Room_Count'])+", room_rate="+str(i['Price'])+"\
+	        WHERE  id="+str(res[0]['id'])+" and room_date='"+i['date']+"'")  
+             
+    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
