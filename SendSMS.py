0d8a3ab25774ecf5167cca46f8566ede9b574028
dpraja <raja.pushpanathan@gmail.com>
2018-05-16 18:23:25 +0530

    Update SendSMS.py

--- SendSMS.py
+++ SendSMS.py
@@ -12,7 +12,8 @@ def sendsms(request):
      arrival = request.json['arrival']
      depature = request.json['depature']
      room_type = request.json['room_type']
-     all_message = ("Dear "+name+", "+message+".  Confirmation Number is "+conf_no+", Arrival Date: "+arrival+", Depature Date:"+depature+", Room Type:"+room_type+". by "+hotel_name+"")     url = "https://control.msg91.com/api/sendhttp.php?authkey=195833ANU0xiap5a708d1f&mobiles="+phone+"&message="+all_message+"&sender=Infoit&route=4&country=91")
+     all_message = ("Dear "+name+", "+message+".  Confirmation Number is "+conf_no+", Arrival Date: "+arrival+", Depature Date:"+depature+", Room Type:"+room_type+". by "+hotel_name+"")     
+     url = "https://control.msg91.com/api/sendhttp.php?authkey=195833ANU0xiap5a708d1f&mobiles="+phone+"&message="+all_message+"&sender=Infoit&route=4&country=91")
      req = urllib.request.Request(url)
      with urllib.request.urlopen(req) as response:
          the_page = response.read()
