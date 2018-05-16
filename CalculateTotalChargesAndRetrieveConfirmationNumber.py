8ae8e7e7b1eeda5538326a000c08e3015f709044
dpraja <raja.pushpanathan@gmail.com>
2018-05-16 15:43:08 +0530

    Update CalculateTotalChargesAndRetrieveConfirmationNumber.py

--- CalculateTotalChargesAndRetrieveConfirmationNumber.py
+++ CalculateTotalChargesAndRetrieveConfirmationNumber.py
@@ -1,65 +1,51 @@
-from sqlwrapper import gensql
+from sqlwrapper import gensql,dbget
 import json
-import random
 import datetime
-def calculatetotalchargesandretrieveconfirmationnumber(request):
-    a = request.json
-    l = { k : v for k,v in a.items() if k in ('customer_mobile','customer_room_type')}
-    if request.args.get('business_id'): 
-       customer_name = request.json["customer_name"]
-    else:
-       customer_name  = "Customer"
-    l['customer_name'] = customer_name   
-    customer_mobile = request.json["customer_mobile"]        
-    customer_arrival_date = request.json["customer_arrival_date"]
-    customer_depature_date = request.json["customer_depature_date"]
-    customer_room_type = request.json["customer_room_type"]
-    d,e,d1,d2 = {},[],{},{}
-    print(customer_arrival_date,customer_depature_date)
-    today_date = datetime.datetime.utcnow().date()
-    year = str(today_date.year)
-    if int(customer_arrival_date[0:2]) == today_date.month :
-        if int(customer_arrival_date[2:]) < today_date.day :
-           year = str(today_date.year+1)
-           print("year",year,type(year))
-    elif int(customer_arrival_date[0:2]) < today_date.month :
-        year = str(today_date.year+1)
-    customer_arrival_date = year+'-'+customer_arrival_date[0:2]+'-'+customer_arrival_date[2:]
-    l['customer_arrival_date'] = customer_arrival_date
-    if int(customer_depature_date[0:2]) == today_date.month :
-        if int(customer_depature_date[2:]) < today_date.day :
-           year = str(today_date.year+1)
-           print("year",year,type(year))
-    elif int(customer_depature_date[0:2]) < today_date.month :
-        year = str(today_date.year+1)
-    customer_depature_date = year+'-'+customer_depature_date[0:2]+'-'+customer_depature_date[2:]
-    l['customer_depature_date'] = customer_depature_date
-    print(customer_arrival_date,customer_depature_date)    
-    e = ['room_count_available','room_rate','currency']
-    d['room_type'] =  customer_room_type
-    result = (gensql('select','ivr_room_availability_rate',e,d))
-    result = json.loads(result)
-    print(result)
-    res = result[0]
-    room_available = res['room_count_available']
-    room_rate = res['room_rate']
-    print(room_rate,type(room_rate))
-    currency = res['currency']
-    if room_available == 0 :
-        return(json.dumps({"Return":"RoomType Not Available ","Return_Code":"RTNA","Status": "Success",
-                      "Status_Code": "200"},indent=2))
-    arrival_date = datetime.datetime.strptime(customer_arrival_date, '%Y-%m-%d')
-    depature_date = datetime.datetime.strptime(customer_depature_date, '%Y-%m-%d')
-    print(arrival_date,depature_date)
-    night = (depature_date - arrival_date).days
-    print(night,type(night))
-    Total_amt = night * room_rate
-    print(Total_amt,type(Total_amt))
-    conf_no = (random.randint(1000000000,9999999999))
-    room_available -= 1
-    l['customer_confirmation_number'] = conf_no
-    d1['room_count_available'] = room_available
-    d2['room_type'] = customer_room_type
-    print(gensql('update','ivr_room_availability_rate',d1,d2))
-    print(gensql('insert','ivr_room_customer_booked',l))    
-    return(json.dumps({"Total_Amount":Total_amt,"currency":currency,"Confirmation_Number":conf_no},indent=2))
+def calculatetotalcharges(request):
+    try:
+        tfn = request.json['TFN']
+        b_id = json.loads(dbget("select id from ivr_dialed_number where dialed_number='"+tfn+"' "))
+        #print(b_id[0]['id'])
+        bi_id = json.loads(dbget("select business_id from ivr_hotel_list where id='"+str(b_id[0]['id'])+"' "))
+        #print(bi_id[0]['business_id'],type(bi_id[0]['business_id']))
+        customer_arrival_date = request.json["customer_arrival_date"]
+        customer_depature_date = request.json["customer_depature_date"]
+        customer_room_type = request.json["customer_room_type"]
+        d,e,d1,d2 = {},[],{},{}
+        #print(customer_arrival_date,customer_depature_date)
+        today_date = datetime.datetime.utcnow().date()
+        year = str(today_date.year)
+        if int(customer_arrival_date[0:2]) == today_date.month :
+            if int(customer_arrival_date[2:]) < today_date.day :
+               year = str(today_date.year+1)
+               print("year",year,type(year))
+        elif int(customer_arrival_date[0:2]) < today_date.month :
+            year = str(today_date.year+1)
+        customer_arrival_date = year+'-'+customer_arrival_date[0:2]+'-'+customer_arrival_date[2:]
+        
+        if int(customer_depature_date[0:2]) == today_date.month :
+            if int(customer_depature_date[2:]) < today_date.day :
+               year = str(today_date.year+1)
+               print("year",year,type(year))
+        elif int(customer_depature_date[0:2]) < today_date.month :
+            year = str(today_date.year+1)
+        customer_depature_date = year+'-'+customer_depature_date[0:2]+'-'+customer_depature_date[2:]
+        
+        #print(customer_arrival_date,customer_depature_date)    
+        result = json.loads(dbget("select standard_rate,standard_rate_currency from extranet_room_list where \
+                                        room_type in ('"+customer_room_type+"') and business_id='"+bi_id[0]['business_id']+"' "))
+        #print("res",result)
+        res = result[0]
+        #print(res)
+        room_rate = res['standard_rate']
+        currency = res['standard_rate_currency']
+        arrival_date = datetime.datetime.strptime(customer_arrival_date, '%Y-%m-%d')
+        depature_date = datetime.datetime.strptime(customer_depature_date, '%Y-%m-%d')
+        #print(arrival_date,depature_date)
+        night = (depature_date - arrival_date).days
+        #print(night,type(night))
+        Total_amt = night * room_rate
+        #print(Total_amt,type(Total_amt))
+        return(json.dumps({"ServiceMessage":"Success","Total_Amount":Total_amt,"currency":currency}))
+    except:
+        return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"}))
