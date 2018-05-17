fcd6a84f6220886df693ac24a6cbd38298a73e4c
dpraja <raja.pushpanathan@gmail.com>
2018-05-17 13:11:15 +0530

    Update InsertCustomerRoomBooking.py

--- InsertCustomerRoomBooking.py
+++ InsertCustomerRoomBooking.py
@@ -1,4 +1,4 @@
-from sqlwrapper import gensql,dbget
+from sqlwrapper import gensql,dbget,dbput
 import json
 import random
 import datetime
@@ -15,7 +15,7 @@ def insertcustomerroombooking(request):
         if len(customer_name) == 0: 
             customer_name  = "Customer"
         e['customer_name'] = customer_name    
-        print(customer_name)
+        #print(customer_name)
         customer_arrival_date = request.json["customer_arrival_date"]
         customer_depature_date = request.json["customer_depature_date"]
         customer_expirydate = request.json["customer_expirydate"]
@@ -39,13 +39,28 @@ def insertcustomerroombooking(request):
             year = str(today_date.year+1)
         customer_depature_date = year+'-'+customer_depature_date[0:2]+'-'+customer_depature_date[2:]
         e['customer_depature_date'] = customer_depature_date
-        print(customer_arrival_date,customer_depature_date)
+        #print(customer_arrival_date,customer_depature_date)
+        arrival_date = datetime.datetime.strptime(customer_arrival_date, '%Y-%m-%d').date()
+        depature_date = datetime.datetime.strptime(customer_depature_date, '%Y-%m-%d').date()        
+        str_date = "'"+str(arrival_date)+"'"
+        while arrival_date < depature_date:
+              arrival_date = arrival_date+datetime.timedelta(days=1)
+              str_date += ","+"'"+str(arrival_date)+"'"
+        #print(str_date)
         conf_no = (random.randint(1000000000,9999999999))
         e['customer_confirmation_number'] = conf_no
         e['customer_booked_status'] = 'booked'
         e['customer_booked_date'] = today_date
         e['id'] = b_id[0]['id']
         print(gensql('insert','ivr_room_customer_booked',e))
+        bi_id = json.loads(dbget("select business_id from ivr_hotel_list where id='"+str(b_id[0]['id'])+"' "))
+        #print(bi_id[0]['business_id'],type(bi_id[0]['business_id']))
+        id_bus = json.loads(dbget("select id from extranet_room_list where business_id ='"+bi_id[0]['business_id']+"' \
+                                   and room_type= '"+e['customer_room_type']+"' "))
+        #print(id_bus[0]['id'])
+        print(dbput("update extranet_availableroom set available_count = available_count-1 where \
+                       id= "+str(id_bus[0]['id'])+" and room_date in ("+str_date+")"))
+        
         return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","conf_no":conf_no}))
      except:
         return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"}))
