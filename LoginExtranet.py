352fa91c60652528fe540ff5045321c7101f2689
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-05-29 16:42:16 +0530

    Update LoginExtranet.py

--- LoginExtranet.py
+++ LoginExtranet.py
@@ -7,11 +7,12 @@ def login(request):
     email_id = { k : v for k,v in d.items() if k != 'user_password'}
     pw = { k : v for k,v in d.items() if k == 'user_password'}
     print(email_id,pw)
-    data = json.loads(gensql('select','extranet_signup','user_password',email_id))
+    data = json.loads(gensql('select','extranet_signup','user_password,business_id',email_id))
     data = data[0]
     print(data,type(data))
     if data['user_password'] == pw['user_password']:
        print("password matched")
-       return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
+       
+       return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Business_id":data['business_id']},indent=2))
 
     return(json.dumps({"ServiceStatus":"Failure","ServiceMessage":"Failure"},indent=2))
